5b828360b8f18eb3bdff704743cb16184321f8c4
Titilope Asare <titilopeasare@gmail.com>
2018-07-16 18:49:24 +0100

    UI design done for options page, next is to add in the logic

--- scripts/option-new.js
+++ scripts/option-new.js
@@ -1,13 +1,44 @@
 (function ($) {
-
     $(function () {
         hookEventHandlers($);
     });
-
 })(jQuery)
 
 function hookEventHandlers($) {
+    groupModalSetup($);
+    //extensionModalSetup($);
+}
+
+function groupModalSetup($) {
     $(".add-group").click(() => {
-        $(".new-group-form").modal("show");
-    })
+        $(".new-extension-form")
+            .modal({
+                closable: false,
+                onDeny: () => {
+                    // clear form field
+                },
+                onApprove: () => {
+                    //approved function call here
+                },
+                transition: 'fade up'
+            })
+            .modal("show");
+    });
+}
+
+function extensionModalSetup($) {
+    $(".add-group").click(() => {
+        $(".new-group-form")
+            .modal({
+                closable: false,
+                onDeny: () => {
+                    // clear form field
+                },
+                onApprove: () => {
+                    //approved function call here
+                },
+                transition: 'fade up'
+            })
+            .modal("show");
+    });
 }
